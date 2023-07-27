15ea1a87db9a2eb6445055483c093d09e4b9a0cf
rahmanrusmana <rahmanrusmana27@gmail.com>
2023-07-27 10:30:28 +0700

    WebProject

--- my-project/src/ContenPageComponent3.vue
+++ my-project/src/ContenPageComponent3.vue
@@ -24,6 +24,8 @@ import { TheCard,Button,Rating } from 'flowbite-vue'
         <p class="ml-2 text-sm font-bold text-gray-900 dark:text-white">4.95</p>
       </template>
     </Rating>
+    <span class="text-lg text-sm text-gray-900 line-through dark:text-white">Rp 2.500.000</span> <br>
+    <span class="text-lg text-base text-red-500">Rp 1.600.000</span>
   
     </the-card></div>
         <div class="basis-1/3 m-2"><the-card variant="image" img-src="https://ik.imagekit.io/tvlk/apr-asset/dgXfoyh24ryQLRcGq00cIdKHRmotrWLNlvG-TxlcLxGkiDwaUSggleJNPRgIHCX6/hotel/asset/10002061-3888x2592-FIT_AND_TRIM-9e0fc9b5c11f3ef35eb426e43074e9ac.jpeg?_src=imagekit&tr=c-at_max,h-569,q-40,w-320" img-alt="Desk">
@@ -38,6 +40,8 @@ import { TheCard,Button,Rating } from 'flowbite-vue'
         <p class="ml-2 text-sm font-bold text-gray-900 dark:text-white">4.95</p>
       </template>
     </Rating>
+    <span class="text-lg text-sm text-gray-900 line-through dark:text-white">Rp 2.500.000</span> <br>
+    <span class="text-lg text-base text-red-500">Rp 1.600.000</span>
   </the-card></div>
         <div class="basis-1/3 m-2"><the-card variant="image" img-src="https://ik.imagekit.io/tvlk/apr-asset/dgXfoyh24ryQLRcGq00cIdKHRmotrWLNlvG-TxlcLxGkiDwaUSggleJNPRgIHCX6/hotel/asset/10000823-1152x768-FIT_AND_TRIM-0e2bf74ec5eb23531d514c76cdc266ae.jpeg?_src=imagekit&tr=c-at_max,h-569,q-40,w-320" img-alt="Desk">
     <h5 class="mb-2 text-1xl font-bold tracking-tight text-gray-900 dark:text-white">The Banyumas Villa</h5>
@@ -51,6 +55,18 @@ import { TheCard,Button,Rating } from 'flowbite-vue'
         <p class="ml-2 text-sm font-bold text-gray-900 dark:text-white">4.95</p>
       </template>
     </Rating>
+    <span class="text-lg text-sm text-gray-900 line-through dark:text-white">Rp 2.500.000</span> <br>
+    <span class="text-lg text-base text-red-500">Rp 1.600.000</span>
   </the-card></div>
     </div>
+    <center><Button color="default" pill outline square>
+    Lihat Semua Hotel
+    <template #suffix>
+      <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    </template>
+  </Button></center>
 </template>
+
+<style>
+  
+</style>
--- my-project/src/NavbarComponent.vue
+++ my-project/src/NavbarComponent.vue
@@ -17,7 +17,8 @@ import { Navbar, NavbarLogo, NavbarCollapse, NavbarLink } from 'flowbite-vue'
         </NavbarCollapse>
       </template>
       <template #right-side>
-        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</button>
+        <button type="button" class="text-blue bg-blue-0 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</button>
+        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign Up</button>
       </template>
     </Navbar>
 </template>
